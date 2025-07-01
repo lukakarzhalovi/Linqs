b39accd5e865e3172159708867995d151cd61082
lkarzhalovi <karzhalovi@gmail.com>
2025-07-01 23:24:24 +0400

    add own select implementation

--- OwnLinqs/Linqs/OwnSelectLinq.cs
+++ OwnLinqs/Linqs/OwnSelectLinq.cs
@@ -1,6 +1,15 @@
 namespace OwnLinqs.Linqs;
 
-public class OwnSelectLinq
+public static class OwnSelectLinq
 {
-    
+    public static IEnumerable<T> OwnSelect<T>(this IEnumerable<T> source, Func<T, T> selector)
+    {
+        ArgumentNullException.ThrowIfNull(source);
+        ArgumentNullException.ThrowIfNull(selector);
+
+        foreach (var eachSource in source)
+        {
+            yield return selector(eachSource);
+        }
+    }
 }
--- OwnLinqs/Program.cs
+++ OwnLinqs/Program.cs
@@ -3,10 +3,15 @@ using OwnLinqs.Linqs;
 
 var list = new List<int> { 1, 2, 34134, 42341, 32123 };
 
-var enumerable = list.MyWhere(i => i >1 );
+var whereEnumerable = list.MyWhere(i => i >1 );
+var selectEnumerable = list.OwnSelect(i => i * 2);
 
-foreach (var each in enumerable)
+foreach (var each in whereEnumerable)
 {
-    Console.WriteLine("ლუკა" + each);
+    Console.WriteLine(each);
 }
 
+foreach (var each in selectEnumerable)
+{
+    Console.WriteLine(each);
+}
